f83ae7b55a1a80c3829f5cc00dfd3869ba7e4478
Loïc Lecrenier <loic.lecrenier@me.com>
2020-01-09 15:49:21 +0100

    Add example and bench templates

---
+++ fuzzcheck_input/benches/integer.rs
@@ -0,0 +1,24 @@
+// #![feature(test)]
+
+// extern crate fuzzcheck_input;
+// extern crate test;
+// use fuzzcheck::input::InputGenerator;
+// use fuzzcheck_input::integer::*;
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use test::Bencher;
+
+//     #[bench]
+//     fn bench_mutate_integer(b: &mut Bencher) {
+//         let mut generator = U8Generator::default();
+//         let mut i = 0;
+//         b.iter(|| {
+//             let mut x = 70;
+//             let mut cplx = 0.0;
+//             generator.mutate(&mut x, i, &mut cplx, 0.0);
+//             i += 1;
+//         });
+//     }
+// }
