c16d0da5476c1665a5446503cc0c933ad275d654
Loïc Lecrenier <loic.lecrenier@me.com>
2020-06-30 16:55:55 +0200

    Update vector mutator example

--- fuzzcheck_mutators/examples/vector_u8.rs
+++ fuzzcheck_mutators/examples/vector_u8.rs
@@ -1,34 +1,47 @@
 extern crate fuzzcheck_mutators;
-use fuzzcheck::Mutator;
+
+use fuzzcheck_traits::Mutator;
+
 use fuzzcheck_mutators::integer::*;
 use fuzzcheck_mutators::vector::*;
 
-extern crate rand;
-use rand::rngs::SmallRng;
-use rand::SeedableRng;
-
 fn main() {
     type M = VecMutator<U8Mutator>;
-    let mut m = M::new(SmallRng::seed_from_u64(0), U8Mutator::default());
+    let mut m = M::default();
 
-    let mut x = vec![0, 167, 200, 103, 56, 78, 2, 127];
-    let mut x_cache = m.cache_from_value(&x);
-    let mut x_step = m.mutation_step_from_value(&x);
+    let x = vec![];
+    let x_cache = m.cache_from_value(&x);
+    let x_step = m.mutation_step_from_value(&x);
 
-    let mut results: Vec<Vec<u8>> = vec![];
-    for _ in 0..100 {
+    let mut results = vec![(x, x_cache, x_step)];
+
+    for i in 0 .. 100 {
+        let (x, x_cache) = m.arbitrary(i, 4096.0);
+        let x_step = m.mutation_step_from_value(&x);
+        results.push((x, x_cache, x_step));
+    }
+
+    for _ in 0..10_000_000 {
+        let len = results.len();
+        let (x, cache, step) = &mut results[fastrand::usize(0..len)];
         let prev_x = x.clone();
+
         // println!("{:?}", x);
-        let token = m.mutate(&mut x, &mut x_cache, &mut x_step, 1000.0);
-        results.push(x.clone());
+        let token = m.mutate(x, cache, step, 4096.0);
+
+        let next = (x.clone(), cache.clone(), m.mutation_step_from_value(x));
+
         // println!("{:?}", x);
-        m.unmutate(&mut x, &mut x_cache, token);
+        m.unmutate(x, cache, token);
         // println!("{:?}", x);
-        assert!(x == prev_x);
+        assert!(x.clone() == prev_x);
+
+        // results.push(next);
     }
-    for x in results.iter() {
-        println!("{:?}", x);
-    }
+    println!("{}", results.len());
+    // for (x, _, _) in results.iter() {
+    //     println!("{:?}", x.len());
+    // }
 
     // results.clear();
 
