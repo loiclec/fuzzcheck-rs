6056932dce614cc754b8a238574ffb2b7976af8f
Loïc Lecrenier <loic.lecrenier@me.com>
2020-07-17 12:27:04 +0200

    Make SerdeSerializer a normal type instead of a macro declaring a type

--- fuzzcheck_serializer/src/lib.rs
+++ fuzzcheck_serializer/src/lib.rs
@@ -3,8 +3,7 @@
 //!
 //! * SerdeSerializer uses the `serde` and `serde_json` crate to serialize
 //! the test inputs (of arbitrary Serializable type) to a `.json` file.
-//! However, SerdeSerializer is not defined in this crate. Instead, it can
-//! be made available through the [define_serde_serializer] macro.
+//! It is available under the “serde” feature
 //!
 //! * [ByteSerializer] encodes and decodes values of type `Vec<u8>` by simply
 //! copy/pasting the bytes from/to the files. The extension is customizable.
@@ -12,58 +11,46 @@
 
 extern crate fuzzcheck_traits;
 
-/// Defines a struct called `SerdeSerializer<T>` that implements the
-/// `fuzzcheck::Serializer` trait using serde.
-///
+#[cfg(feature = "serde_serializer")]
+extern crate serde;
+#[cfg(feature = "serde_serializer")]
+extern crate serde_json;
+
+#[cfg(feature = "serde_serializer")]
+use std::marker::PhantomData;
+
 /// `SerdeSerializer<T>` uses `serde` and `serde_json` to serialize the test
 /// inputs (of arbitrary type `T: Serializable+ for<'e> Deserializable<'e>`)
 /// to a json file.
-///
-/// This macro only works if `serde`, `serde_json`, and `fuzzcheck` are
-/// accessible.
-///
-/// ## Example
-///
-/// ```ignore
-/// extern crate serde;
-/// extern crate serde_json;
-/// extern crate fuzzcheck;
-///
-/// define_serde_serializer!();
-///
-/// let serializer = SerdeSerializer::<T>::default();
-/// ```
-#[macro_export]
-macro_rules! define_serde_serializer {
-    () => {
-        pub struct SerdeSerializer<S> {
-            phantom: std::marker::PhantomData<S>,
-        }
+#[cfg(feature = "serde_serializer")]
+pub struct SerdeSerializer<S> {
+    phantom: PhantomData<S>,
+}
 
-        impl<S> Default for SerdeSerializer<S> {
-            fn default() -> Self {
-                Self {
-                    phantom: std::marker::PhantomData,
-                }
-            }
+#[cfg(feature = "serde_serializer")]
+impl<S> Default for SerdeSerializer<S> {
+    fn default() -> Self {
+        Self {
+            phantom: PhantomData,
         }
+    }
+}
 
-        impl<S> fuzzcheck_traits::Serializer for SerdeSerializer<S>
-        where
-            S: serde::Serialize + for<'e> serde::Deserialize<'e>,
-        {
-            type Value = S;
-            fn extension(&self) -> &str {
-                "json"
-            }
-            fn from_data(&self, data: &[u8]) -> Option<S> {
-                serde_json::from_slice(data).ok()
-            }
-            fn to_data(&self, value: &Self::Value) -> Vec<u8> {
-                serde_json::to_vec(value).unwrap()
-            }
-        }
-    };
+#[cfg(feature = "serde_serializer")]
+impl<S> fuzzcheck_traits::Serializer for SerdeSerializer<S>
+where
+    S: serde::Serialize + for<'e> serde::Deserialize<'e>,
+{
+    type Value = S;
+    fn extension(&self) -> &str {
+        "json"
+    }
+    fn from_data(&self, data: &[u8]) -> Option<S> {
+        serde_json::from_slice(data).ok()
+    }
+    fn to_data(&self, value: &Self::Value) -> Vec<u8> {
+        serde_json::to_vec(value).unwrap()
+    }
 }
 
 /**
