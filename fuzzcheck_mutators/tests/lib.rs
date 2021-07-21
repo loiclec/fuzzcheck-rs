6d89df452a7ca230ba6d3d8567e8de944ae1b41b
Loïc Lecrenier <loic.lecrenier@icloud.com>
2021-07-21 20:47:32 +0200

    Remove useless file in integration tests of fuzzcheck_mutator

--- fuzzcheck_mutators/tests/lib.rs
+++ fuzzcheck_mutators/tests/lib.rs
@@ -1,10 +1,10 @@
-#![feature(arc_new_cyclic)]
-use fuzzcheck_traits::Mutator;
-//
-//
-mod alternation_char_mutators;
-mod char_mutators;
-mod constrained_integer;
-mod expansions;
-mod option;
-mod vector;
+// #![feature(arc_new_cyclic)]
+// use fuzzcheck_traits::Mutator;
+
+// //
+// mod alternation_char_mutators;
+// mod char_mutators;
+// mod constrained_integer;
+// mod expansions;
+// mod option;
+// mod vector;
