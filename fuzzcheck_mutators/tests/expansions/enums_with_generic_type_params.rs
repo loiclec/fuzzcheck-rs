012b2a4eedab7a1f5e89ff503b1dc36632eddfd8
Loïc Lecrenier <loic.lecrenier@icloud.com>
2021-07-21 15:24:26 +0200

    Restore a fuzzcheck_mutators_derive test

--- fuzzcheck_mutators/tests/expansions/enums_with_generic_type_params.rs
+++ fuzzcheck_mutators/tests/expansions/enums_with_generic_type_params.rs
@@ -1,5 +1,5 @@
 use fuzzcheck_mutators::fuzzcheck_traits::Mutator;
-use fuzzcheck_mutators::{DefaultMutator, U8Mutator, VecMutator};
+use fuzzcheck_mutators::DefaultMutator;
 
 #[derive(Clone, Debug, DefaultMutator)]
 pub enum X<T> {
@@ -9,18 +9,12 @@ pub enum X<T> {
 
 #[test]
 fn test_compile() {
-    // let m = X::<Vec<u8>>::default_mutator();
-    // let (value, _cache): (X<Vec<u8>>, _) = m.random_arbitrary(10.0);
+    let m = X::<Vec<u8>>::default_mutator();
+    let (value, _cache): (X<Vec<u8>>, _) = m.random_arbitrary(100.0);
 
-    // match &value {
-    //     X::A(_x) => {}
-    //     X::B(_y) => {}
-    // }
-    // println!("{:?}", value);
-    // let m = VecMutator::new(VecMutator::new(U8Mutator::default(), 2..=1000), 4..=10);
-    let m = Vec::<Vec<u8>>::default_mutator();
-
-    println!("{}", m.min_complexity());
-    let (value, _cache): (Vec<Vec<u8>>, _) = m.random_arbitrary(1000.0);
-    println!("{:?} {}", value, value.len());
+    match &value {
+        X::A(_x) => {}
+        X::B(_y) => {}
+    }
+    println!("{:?}", value);
 }
