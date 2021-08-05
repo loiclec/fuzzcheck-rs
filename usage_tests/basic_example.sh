c42b39e734fd0495c0f18c0763db0d59b3129a33
Loïc Lecrenier <loic.lecrenier@icloud.com>
2021-08-05 13:49:10 +0200

    Remove usage tests of cargo-fuzzcheck

--- usage_tests/basic_example.sh
+++
@@ -1,7 +0,0 @@
-FUZZCHECK_RS="$PWD"
-cargo install --path cargo-fuzzcheck --force && \
-cd usage_tests && \
-cargo new --lib basic_example && \
-cd basic_example && \
-cargo fuzzcheck init file://"$FUZZCHECK_RS" && \
-cargo fuzzcheck run target1 fuzz
