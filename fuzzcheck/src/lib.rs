222a7950dccbe220e39ca289f5cc145143fb8aa2
Loïc Lecrenier <loic.lecrenier@icloud.com>
2021-11-19 09:28:46 +0100

    Remove unused nightly feature gates

--- fuzzcheck/src/lib.rs
+++ fuzzcheck/src/lib.rs
@@ -17,8 +17,6 @@
 #![feature(generic_associated_types)]
 #![feature(variant_count)]
 #![feature(arc_new_cyclic)]
-#![feature(box_syntax)]
-#![feature(trivial_bounds)]
 #![allow(clippy::nonstandard_macro_braces)]
 #![allow(clippy::too_many_arguments)]
 
